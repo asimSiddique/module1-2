33b997f9ba29061f289db2318b92b389304e2c75
Asim Siddique <asim.siddiqm@gmail.com>
2022-01-24 16:31:04 -0500

    Adding Module 2 Excercise 1

---
+++ Module2_Excercise_1.py
@@ -0,0 +1,8 @@
+#Group Members 
+#Sajal Baral, Shawn Kiruba, Kevin Avila, Shail Patel
+# get input, reverse a string. join that string and print it. 
+
+user_input = input("Enter a string: ")
+reversed_string = "".join((reversed(user_input)))
+print(reversed_string)
+
